34213da72d92de7042fa000ce31a4a934a3b5761
wanghao <wanghao@ninghao.net>
2021-06-07 18:04:16 +0800

    显示资源包里的图像

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -4,26 +4,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: RichText(
-        textDirection: TextDirection.ltr,
-        text: TextSpan(
-          text: '犬吠水声中，',
-          style: TextStyle(
-            fontSize: 22.0,
-          ),
-          children: [
-            TextSpan(
-              text: '桃花',
-              style: TextStyle(
-                color: Colors.pinkAccent,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            TextSpan(
-              text: '带露浓。',
-            ),
-          ],
-        ),
+      child: Image.asset(
+        'assets/images/logo.png',
+        width: 128,
       ),
     );
   }
