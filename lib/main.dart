2a0bf67ae613a1565dcec62dae382ea0096e08ad
wanghao <wanghao@ninghao.net>
2021-06-07 14:20:25 +0800

    准备应用入口

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(
+    Center(
+      child: Text(
+        '宁皓网',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
