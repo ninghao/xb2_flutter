e7038e6479bfd6d89369b7c98ce62893d4e9ec18
wanghao <wanghao@ninghao.net>
2021-06-08 07:02:28 +0800

    创建 Material 应用

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -3,58 +3,10 @@ import 'package:flutter/material.dart';
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Image.asset(
-          'assets/images/logo.png',
-          width: 96,
-          color: Colors.white,
-        ),
-        // color: Colors.deepPurpleAccent,
-        padding: EdgeInsets.all(60),
-        alignment: Alignment.topCenter,
-        width: 350,
-        height: 350,
-        // transform: Matrix4.rotationZ(0.5),
-        // transformAlignment: Alignment.topRight,
-        decoration: BoxDecoration(
-          color: Colors.deepPurpleAccent,
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Colors.yellow,
-              Colors.pink,
-              Colors.blue,
-              Colors.cyan,
-            ],
-          ),
-          image: DecorationImage(
-            image: NetworkImage(
-              'https://resources.ninghao.net/images/IMG_2626.JPG',
-            ),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-              Colors.deepOrange,
-              BlendMode.softLight,
-            ),
-          ),
-          borderRadius: BorderRadius.all(
-            Radius.circular(25),
-          ),
-          border: Border.all(
-            color: Colors.deepOrangeAccent,
-            width: 5,
-            style: BorderStyle.solid,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.deepOrangeAccent,
-              offset: Offset(5, 20),
-              blurRadius: 30,
-            ),
-          ],
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text('NINGHAO'),
       ),
     );
   }
