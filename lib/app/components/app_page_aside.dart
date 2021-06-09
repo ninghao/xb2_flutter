a52096d240eacfb24bf043e391c0f87d4c2635eb
wanghao <wanghao@ninghao.net>
2021-06-09 14:24:01 +0800

    设置边栏抽屉上显示的内容（ListView 与 ListTile）

--- lib/app/components/app_page_aside.dart
+++ lib/app/components/app_page_aside.dart
@@ -4,8 +4,81 @@ class AppPageAside extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: Center(
-        child: Text('AppPageAside'),
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          UserAccountsDrawerHeader(
+            accountName: Text('王皓'),
+            accountEmail: Text('wanghao@ninghao.net'),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                'https://resources.ninghao.net/wanghao.jpg',
+              ),
+            ),
+          ),
+          ListTile(
+            title: Text(
+              '作品',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(
+              Icons.image_outlined,
+              color: Colors.black26,
+              size: 22,
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text(
+              '评论',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(
+              Icons.comment_outlined,
+              color: Colors.black26,
+              size: 22,
+            ),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            title: Text(
+              '帐户',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(
+              Icons.manage_accounts_outlined,
+              color: Colors.black26,
+              size: 22,
+            ),
+            onTap: () {},
+          ),
+          ListTile(
+            title: Text(
+              '管理',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(
+              Icons.collections_outlined,
+              color: Colors.black26,
+              size: 22,
+            ),
+            onTap: () {},
+          ),
+          Divider(),
+          ListTile(
+            title: Text(
+              '退出',
+              textAlign: TextAlign.right,
+            ),
+            trailing: Icon(
+              Icons.logout_outlined,
+              color: Colors.black26,
+              size: 22,
+            ),
+            onTap: () {},
+          ),
+        ],
       ),
     );
   }
