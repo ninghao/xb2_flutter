cda9f85f8873c15db2d30ecf507a2363a7fbd2c0
wanghao <wanghao@ninghao.net>
2021-06-09 12:43:39 +0800

    显示警告对话框（AlertDialog）

--- lib/app/components/app_bottom_sheet.dart
+++ lib/app/components/app_bottom_sheet.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
 
 class AppBottomSheet extends StatelessWidget {
+  // 显示对话框
+  Future<bool?> showAppAlertDialog(context) {
+    return showDialog<bool>(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('确定提交'),
+        content: Text('提交以后无法恢复，确定要提交吗？'),
+        actions: [
+          TextButton(
+            child: Text('取消'),
+            onPressed: () => Navigator.pop(context, false),
+          ),
+          TextButton(
+            child: Text('确定'),
+            onPressed: () => Navigator.pop(context, true),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,7 +37,14 @@ class AppBottomSheet extends StatelessWidget {
         ],
       ),
       child: Center(
-        child: Text('AppBottomSheet'),
+        child: TextButton(
+          child: Text('提交'),
+          onPressed: () async {
+            // 显示对话框
+            final result = await showAppAlertDialog(context);
+            print('showAppAlertDialog: $result');
+          },
+        ),
       ),
     );
   }
