35c7142297fd11559329106403a12b3268d60105
wanghao <wanghao@ninghao.net>
2021-08-03 10:59:03 +0800

    理解小部件的约束

--- lib/playground/layout/playground_layout.dart
+++ lib/playground/layout/playground_layout.dart
@@ -3,6 +3,17 @@ import 'package:flutter/material.dart';
 class PlaygroundLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.greenAccent,
+      width: 200,
+      height: 200,
+      child: Center(
+        child: Container(
+          color: Colors.deepPurpleAccent,
+          width: 100,
+          height: 100,
+        ),
+      ),
+    );
   }
 }
