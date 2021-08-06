0a301fc344109d603aa4274401118319a963fa2d
wanghao <wanghao@ninghao.net>
2021-08-06 09:56:18 +0800

    准备练习演示用户输入的小部件

---
+++ lib/playground/input/playground_input.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:xb2_flutter/user/create/user_create.dart';
+
+class PlaygroundInput extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(32),
+      child: UserCreate(),
+    );
+  }
+}
--- lib/playground/playground.dart
+++ lib/playground/playground.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:xb2_flutter/playground/layout/playground_layout.dart';
+import 'package:xb2_flutter/playground/input/playground_input.dart';
+// import 'package:xb2_flutter/playground/layout/playground_layout.dart';
 
 class Playground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PlaygroundLayout();
+    return PlaygroundInput();
   }
 }
