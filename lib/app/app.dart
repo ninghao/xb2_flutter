382bb2607150b86c540ba354bcb1ffac626926e8
wanghao <wanghao@ninghao.net>
2021-08-08 12:56:45 +0800

    理解 MaterialApp 的 home 属性

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:xb2_flutter/app/components/app_floating_action_button.dart';
-import 'package:xb2_flutter/app/components/app_page_aside.dart';
-import 'package:xb2_flutter/app/components/app_page_bottom.dart';
-import 'package:xb2_flutter/app/components/app_page_header.dart';
-import 'package:xb2_flutter/app/components/app_page_main.dart';
+import 'package:xb2_flutter/app/components/app_home.dart';
 import 'package:xb2_flutter/app/themes/app_theme.dart';
 
 class App extends StatefulWidget {
@@ -12,42 +8,13 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  // 底部导航栏当前项目
-  int currentAppBottomNavigationBarItem = 0;
-
-  // 是否显示应用栏
-  bool showAppBar = true;
-
-  // 点按底部导航栏事件处理
-  void onTapAppBottomNavigationBarItem(int index) {
-    setState(() {
-      currentAppBottomNavigationBarItem = index;
-      showAppBar = index == 0;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
-      home: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          backgroundColor: Colors.amber,
-          appBar: showAppBar ? AppPageHeader() : null,
-          body: AppPageMain(
-            currentIndex: currentAppBottomNavigationBarItem,
-          ),
-          bottomNavigationBar: AppPageBottom(
-            currentIndex: currentAppBottomNavigationBarItem,
-            onTap: onTapAppBottomNavigationBarItem,
-          ),
-          // floatingActionButton: AppFloatingActionButton(),
-          drawer: AppPageAside(),
-        ),
-      ),
+      home: AppHome(),
     );
   }
 }
---
+++ lib/app/components/app_home.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import 'app_page_aside.dart';
+import 'app_page_bottom.dart';
+import 'app_page_header.dart';
+import 'app_page_main.dart';
+
+class AppHome extends StatefulWidget {
+  @override
+  _AppHomeState createState() => _AppHomeState();
+}
+
+class _AppHomeState extends State<AppHome> {
+  // 底部导航栏当前项目
+  int currentAppBottomNavigationBarItem = 0;
+
+  // 是否显示应用栏
+  bool showAppBar = true;
+
+  // 点按底部导航栏事件处理
+  void onTapAppBottomNavigationBarItem(int index) {
+    setState(() {
+      currentAppBottomNavigationBarItem = index;
+      showAppBar = index == 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: Colors.amber,
+        appBar: showAppBar ? AppPageHeader() : null,
+        body: AppPageMain(
+          currentIndex: currentAppBottomNavigationBarItem,
+        ),
+        bottomNavigationBar: AppPageBottom(
+          currentIndex: currentAppBottomNavigationBarItem,
+          onTap: onTapAppBottomNavigationBarItem,
+        ),
+        // floatingActionButton: AppFloatingActionButton(),
+        drawer: AppPageAside(),
+      ),
+    );
+  }
+}
