290cb9014146944653d50696d5a5dc24067aa2e1
wanghao <wanghao@ninghao.net>
2021-08-08 10:41:29 +0800

    准备导航与路由演示小部件

--- lib/playground/playground.dart
+++ lib/playground/playground.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:xb2_flutter/playground/input/playground_input.dart';
+import 'package:xb2_flutter/playground/routing/playground_routing.dart';
+// import 'package:xb2_flutter/playground/input/playground_input.dart';
 // import 'package:xb2_flutter/playground/layout/playground_layout.dart';
 
 class Playground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return PlaygroundInput();
+    return PlaygroundRouting();
   }
 }
