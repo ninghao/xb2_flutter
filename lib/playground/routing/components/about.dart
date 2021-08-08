352ff39105bc77610e2d1d6bc8c936a7eda6e87d
wanghao <wanghao@ninghao.net>
2021-08-08 11:52:41 +0800

    用 Navigator 命令式管理路由（Navigator.push 与 Navigator.pop）

---
+++ lib/playground/routing/components/about.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class About extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              '宁皓网，创立于 2011 年。',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            TextButton(
+              child: Text('返回'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/playground/routing/playground_routing.dart
+++ lib/playground/routing/playground_routing.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:xb2_flutter/playground/routing/components/about.dart';
 
 class PlaygroundRouting extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
+      child: Center(
+        child: TextButton(
+          child: Text('查看宁皓网介绍'),
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(
+              builder: (context) {
+                return About();
+              },
+            ));
+          },
+        ),
+      ),
     );
   }
 }
