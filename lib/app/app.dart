c3b37bda141c2f03566504b82929248f77214318
wanghao <wanghao@ninghao.net>
2021-08-08 15:35:31 +0800

    定义路由表使用带名字的路由

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:xb2_flutter/app/components/app_home.dart';
 import 'package:xb2_flutter/app/themes/app_theme.dart';
+import 'package:xb2_flutter/playground/routing/components/about.dart';
 
 class App extends StatefulWidget {
   @override
@@ -14,7 +15,11 @@ class _AppState extends State<App> {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.light,
       darkTheme: AppTheme.dark,
-      home: AppHome(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => AppHome(),
+        '/about': (context) => About(),
+      },
     );
   }
 }
--- lib/playground/routing/playground_routing.dart
+++ lib/playground/routing/playground_routing.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:xb2_flutter/playground/routing/components/about.dart';
 
 class PlaygroundRouting extends StatelessWidget {
   @override
@@ -10,11 +9,7 @@ class PlaygroundRouting extends StatelessWidget {
         child: TextButton(
           child: Text('查看宁皓网介绍'),
           onPressed: () {
-            Navigator.push(context, MaterialPageRoute(
-              builder: (context) {
-                return About();
-              },
-            ));
+            Navigator.pushNamed(context, '/about');
           },
         ),
       ),
