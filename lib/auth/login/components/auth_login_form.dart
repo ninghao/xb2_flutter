03ae1eb765f8d107f4a6c50f06022fcb69678447
wanghao <wanghao@ninghao.net>
2021-08-10 20:46:02 +0800

    在小部件里使用来自 Provider 提供的数据与方法（Provider.of）

--- lib/auth/login/components/auth_login_form.dart
+++ lib/auth/login/components/auth_login_form.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xb2_flutter/auth/auth_model.dart';
 
 class AuthLoginForm extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final authModel = Provider.of<AuthModel>(context);
+
     final loginButton = ElevatedButton(
       child: Text('登录'),
-      onPressed: () {},
+      onPressed: () {
+        print('已登录：${authModel.isLoggedIn}');
+        authModel.login();
+        print('已登录：${authModel.isLoggedIn}');
+      },
     );
 
     return Container(
