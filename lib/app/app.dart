f1f1ff98571c29d2d782a22140067dbe41daed94
wanghao <wanghao@ninghao.net>
2021-08-10 20:36:15 +0800

    用 Provider 提供数据与方法

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:xb2_flutter/app/components/app_home.dart';
 import 'package:xb2_flutter/app/themes/app_theme.dart';
+import 'package:xb2_flutter/auth/auth_model.dart';
 import 'package:xb2_flutter/playground/routing/components/about.dart';
 import 'package:xb2_flutter/post/show/post_show.dart';
 
@@ -12,34 +14,38 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.light,
-      darkTheme: AppTheme.dark,
-      initialRoute: '/',
-      // routes: {
-      //   '/': (context) => AppHome(),
-      //   '/about': (context) => About(),
-      // },
-      onGenerateRoute: (settings) {
-        print(settings);
+    return Provider(
+      create: (context) => AuthModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.light,
+        darkTheme: AppTheme.dark,
+        initialRoute: '/',
+        // routes: {
+        //   '/': (context) => AppHome(),
+        //   '/about': (context) => About(),
+        // },
+        onGenerateRoute: (settings) {
+          print(settings);
 
-        if (settings.name == '/') {
-          return MaterialPageRoute(builder: (context) => AppHome());
-        }
+          if (settings.name == '/') {
+            return MaterialPageRoute(builder: (context) => AppHome());
+          }
 
-        if (settings.name == '/about') {
-          return MaterialPageRoute(builder: (context) => About());
-        }
+          if (settings.name == '/about') {
+            return MaterialPageRoute(builder: (context) => About());
+          }
 
-        final uri = Uri.parse(settings.name ?? '');
+          final uri = Uri.parse(settings.name ?? '');
 
-        // posts/:postId
-        if (uri.pathSegments.length == 2 && uri.pathSegments.first == 'posts') {
-          final postId = uri.pathSegments[1];
-          return MaterialPageRoute(builder: (context) => PostShow(postId));
-        }
-      },
+          // posts/:postId
+          if (uri.pathSegments.length == 2 &&
+              uri.pathSegments.first == 'posts') {
+            final postId = uri.pathSegments[1];
+            return MaterialPageRoute(builder: (context) => PostShow(postId));
+          }
+        },
+      ),
     );
   }
 }
