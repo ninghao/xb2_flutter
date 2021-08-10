6298918b8d87df491ea4b40c816335498777e35d
wanghao <wanghao@ninghao.net>
2021-08-10 21:42:39 +0800

    在小部件里使用来自 Provider 提供的数据与方法（Consumer）

--- lib/user/profile/user_profile.dart
+++ lib/user/profile/user_profile.dart
@@ -1,14 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xb2_flutter/auth/auth_model.dart';
 
 class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Icon(
-        Icons.account_circle_outlined,
-        size: 128,
-        color: Colors.black12,
-      ),
+    return Consumer<AuthModel>(
+      builder: (context, state, child) {
+        return Container(
+          color: Colors.white,
+          height: double.infinity,
+          width: double.infinity,
+          child: Center(
+            child: state.isLoggedIn ? Text(state.name) : Text('未登录'),
+          ),
+        );
+      },
     );
   }
 }
