43ff3fb7d9998a7a7513ad577833dacc8886695d
wanghao <wanghao@ninghao.net>
2021-08-12 13:33:20 +0800

    使用路由器管理路由（Router）

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:xb2_flutter/app/app_model.dart';
 import 'package:xb2_flutter/app/components/app_home.dart';
+import 'package:xb2_flutter/app/router/app_router_delegate.dart';
 import 'package:xb2_flutter/app/themes/app_theme.dart';
 import 'package:xb2_flutter/auth/auth_model.dart';
 import 'package:xb2_flutter/playground/routing/components/about.dart';
@@ -13,41 +14,22 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  final AppModel appModel = AppModel();
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider<AuthModel>(create: (context) => AuthModel()),
-        ChangeNotifierProvider<AppModel>(create: (context) => AppModel()),
+        ChangeNotifierProvider<AppModel>(create: (context) => appModel),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: AppTheme.light,
         darkTheme: AppTheme.dark,
         initialRoute: '/',
-        home: Consumer<AppModel>(
-          builder: (context, state, child) => Navigator(
-            pages: [
-              MaterialPage(
-                key: ValueKey('AppHome'),
-                child: AppHome(),
-              ),
-              if (state.pageName == 'About')
-                MaterialPage(
-                  key: ValueKey('About'),
-                  child: About(),
-                ),
-            ],
-            onPopPage: (route, result) {
-              if (!route.didPop(result)) {
-                return false;
-              }
-
-              state.setPageName('');
-
-              return true;
-            },
-          ),
+        home: Router(
+          routerDelegate: AppRouterDelegate(appModel),
         ),
       ),
     );
--- lib/app/router/app_router_delegate.dart
+++ lib/app/router/app_router_delegate.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:xb2_flutter/app/app_model.dart';
+import 'package:xb2_flutter/app/components/app_home.dart';
+import 'package:xb2_flutter/playground/routing/components/about.dart';
 
 class AppRouterDelegate extends RouterDelegate
     with ChangeNotifier, PopNavigatorRouterDelegateMixin {
   final _navigatorKey;
+  final AppModel appModel;
 
-  AppRouterDelegate() : _navigatorKey = GlobalKey<NavigatorState>();
+  AppRouterDelegate(this.appModel)
+      : _navigatorKey = GlobalKey<NavigatorState>();
 
   // 获取 Navigator 使用的 Key
   @override
@@ -22,6 +27,26 @@ class AppRouterDelegate extends RouterDelegate
   Widget build(BuildContext context) {
     return Navigator(
       key: _navigatorKey,
+      pages: [
+        MaterialPage(
+          key: ValueKey('AppHome'),
+          child: AppHome(),
+        ),
+        if (appModel.pageName == 'About')
+          MaterialPage(
+            key: ValueKey('About'),
+            child: About(),
+          ),
+      ],
+      onPopPage: (route, result) {
+        if (!route.didPop(result)) {
+          return false;
+        }
+
+        appModel.setPageName('');
+
+        return true;
+      },
     );
   }
 }
