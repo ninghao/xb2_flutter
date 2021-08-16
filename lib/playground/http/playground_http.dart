8e5b1b5793680716ef9b9f84ed0f1f6932f98b39
wanghao <wanghao@ninghao.net>
2021-08-16 13:37:20 +0800

    请求服务端接口创建内容（用户）

--- lib/playground/http/playground_http.dart
+++ lib/playground/http/playground_http.dart
@@ -6,7 +6,7 @@ import 'package:xb2_flutter/user/user.dart';
 
 class PlaygroundHttp extends StatelessWidget {
   getUser() async {
-    final uri = Uri.parse('https://nid-node.ninghao.co/users/1');
+    final uri = Uri.parse('https://nid-node.ninghao.co/users/21');
     final response = await http.get(uri);
 
     print('状态码 ${response.statusCode}');
@@ -18,6 +18,21 @@ class PlaygroundHttp extends StatelessWidget {
       print('解码之后 $user');
       print('id: ${user.id}, name: ${user.name}');
     }
+  }
+
+  createUser() async {
+    final name = '王二小';
+    final password = '123456';
+
+    final uri = Uri.parse('https://nid-node.ninghao.co/users');
+
+    final response = await http.post(uri, body: {
+      'name': name,
+      'password': password,
+    });
+
+    print('状态码 ${response.statusCode}');
+    print('响应主体 ${response.body}');
   }
 
   @override
@@ -32,6 +47,10 @@ class PlaygroundHttp extends StatelessWidget {
           ElevatedButton(
             child: Text('发送请求'),
             onPressed: getUser,
+          ),
+          ElevatedButton(
+            child: Text('创建用户'),
+            onPressed: createUser,
           ),
         ],
       ),
