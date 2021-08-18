3f6c1706db88fc61584ba5390d2c6540914f5f2e
wanghao <wanghao@ninghao.net>
2021-08-18 16:53:34 +0800

    定义并提供获取内容列表数据用的方法

--- lib/post/index/components/post_list.dart
+++ lib/post/index/components/post_list.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xb2_flutter/post/index/post_index_model.dart';
 
 class PostList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final model = context.watch<PostIndexModel>();
+    print(model.posts);
+
     return Container();
   }
 }
---
+++ lib/post/index/post_index_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:xb2_flutter/app/app_config.dart';
+
+class PostIndexModel extends ChangeNotifier {
+  dynamic posts;
+
+  getPosts() async {
+    final uri = Uri.parse('${AppConfig.apiBaseUrl}/posts');
+    final response = await http.get(uri);
+
+    posts = response.body;
+    notifyListeners();
+  }
+}
