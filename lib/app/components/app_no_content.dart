d49175a4d59b37b38b96f9e773868beb66613cd4
wanghao <wanghao@ninghao.net>
2021-08-23 17:30:29 +0800

    请求内容页面需要的数据

---
+++ lib/app/components/app_no_content.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppNoContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        child: Text('暂无内容'),
+      ),
+    );
+  }
+}
--- lib/post/show/post_show.dart
+++ lib/post/show/post_show.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xb2_flutter/app/components/app_no_content.dart';
 import 'package:xb2_flutter/post/post.dart';
 import 'package:xb2_flutter/post/show/components/post_show_main.dart';
+import 'package:xb2_flutter/post/show/post_show_model.dart';
 
-class PostShow extends StatelessWidget {
+class PostShow extends StatefulWidget {
   final String postId;
   final Post? post;
 
@@ -12,9 +15,29 @@ class PostShow extends StatelessWidget {
   });
 
   @override
+  _PostShowState createState() => _PostShowState();
+}
+
+class _PostShowState extends State<PostShow> {
+  @override
+  void initState() {
+    super.initState();
+
+    if (widget.post == null) {
+      context.read<PostShowModel>().getPostById(widget.postId);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    Post? post = widget.post;
+
+    if (widget.post == null) {
+      post = context.watch<PostShowModel>().post;
+    }
+
     return Scaffold(
-      body: PostShowMain(post: post!),
+      body: post != null ? PostShowMain(post: post) : AppNoContent(),
     );
   }
 }
