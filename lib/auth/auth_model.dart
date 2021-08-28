c98d62c7a51c83977879c775b06433a3421e521c
wanghao <wanghao@ninghao.net>
2021-08-28 14:16:34 +0800

    定义身份验证模型（AuthModel）

--- lib/auth/auth_model.dart
+++ lib/auth/auth_model.dart
@@ -1,20 +1,40 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:xb2_flutter/app/app_config.dart';
+import 'package:xb2_flutter/app/exceptions/http_exception.dart';
+import 'package:xb2_flutter/auth/auth.dart';
 
 class AuthModel extends ChangeNotifier {
-  bool isLoggedIn = false;
+  String userId = '';
   String name = '';
+  String token = '';
+  bool get isLoggedIn => token.isNotEmpty;
 
-  login() {
-    isLoggedIn = true;
-    name = 'wanghao';
-    print('请求登录！');
-    notifyListeners();
+  Future<Auth> login(LoginData data) async {
+    final uri = Uri.parse('${AppConfig.apiBaseUrl}/login');
+    final response = await http.post(uri, body: data.toJson());
+    final responseBody = jsonDecode(response.body);
+
+    if (response.statusCode == 200) {
+      final auth = Auth.fromJson(responseBody);
+
+      userId = auth.id.toString();
+      name = auth.name;
+      token = auth.token;
+      notifyListeners();
+
+      return auth;
+    } else {
+      throw HttpException(responseBody['message'] ?? '网络请求出了点问题 🌋');
+    }
   }
 
   logout() {
-    isLoggedIn = false;
+    userId = '';
     name = '';
-    print('退出登录！');
+    token = '';
     notifyListeners();
   }
 }
