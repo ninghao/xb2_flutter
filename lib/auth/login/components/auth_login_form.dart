8e4bcc5cc5dd7f9bdf77cdff2fafcb57f0e73c0e
wanghao <wanghao@ninghao.net>
2021-08-28 14:35:02 +0800

    请求用户登录

--- lib/auth/login/components/auth_login_form.dart
+++ lib/auth/login/components/auth_login_form.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:xb2_flutter/app/app_model.dart';
 import 'package:xb2_flutter/app/components/app_button.dart';
 import 'package:xb2_flutter/app/components/app_header_text.dart';
 import 'package:xb2_flutter/app/components/app_password_field.dart';
 import 'package:xb2_flutter/app/components/app_text_field.dart';
+import 'package:xb2_flutter/app/exceptions/http_exception.dart';
+import 'package:xb2_flutter/auth/auth.dart';
 import 'package:xb2_flutter/auth/auth_model.dart';
 
 class AuthLoginForm extends StatefulWidget {
@@ -20,6 +23,7 @@ class _AuthLoginFormState extends State<AuthLoginForm> {
   @override
   Widget build(BuildContext context) {
     final authModel = Provider.of<AuthModel>(context, listen: true);
+    final appModel = context.read<AppModel>();
 
     // 标题
     final header = AppHeaderText('用户登录');
@@ -41,7 +45,24 @@ class _AuthLoginFormState extends State<AuthLoginForm> {
 
     // 提交
     final submitButton = AppButton(
-      onPressed: () {},
+      onPressed: () async {
+        formKey.currentState!.validate();
+
+        try {
+          await authModel.login(
+            LoginData(
+              name: name!,
+              password: password!,
+            ),
+          );
+
+          appModel.setPageName('');
+        } on HttpException catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(e.message)),
+          );
+        }
+      },
       text: '确定登录',
     );
 
