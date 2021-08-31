81f0fed14517dcc1c6069247a191c0f6aee57c74
wanghao <wanghao@ninghao.net>
2021-08-31 20:32:51 +0800

    处理用户取消点赞动作

--- lib/post/components/post_actions.dart
+++ lib/post/components/post_actions.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:xb2_flutter/app/exceptions/http_exception.dart';
 import 'package:xb2_flutter/like/create/like_create_model.dart';
+import 'package:xb2_flutter/like/destroy/like_destroy_model.dart';
 import 'package:xb2_flutter/post/post.dart';
 
 class PostActions extends StatelessWidget {
@@ -15,20 +16,39 @@ class PostActions extends StatelessWidget {
     post.totalLikes = post.totalLikes! + 1;
   }
 
+  decreaseTotalLikes() {
+    post.totalLikes = post.totalLikes != 0 ? post.totalLikes! - 1 : 0;
+  }
+
   liked() {
     post.liked = 1;
     increaseTotalLikes();
   }
 
+  unliked() {
+    post.liked = 0;
+    decreaseTotalLikes();
+  }
+
   @override
   Widget build(BuildContext context) {
     final likeCreateModel = context.watch<LikeCreateModel>();
+    final likeDestroyModel = context.watch<LikeDestroyModel>();
 
     onTapLikeAction() async {
       if (post.liked == 0) {
         try {
           await likeCreateModel.createUserLikePost(post.id!);
           liked();
+        } on HttpException catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(e.message)),
+          );
+        }
+      } else {
+        try {
+          await likeDestroyModel.deleteUserLikePost(post.id!);
+          unliked();
         } on HttpException catch (e) {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(content: Text(e.message)),
