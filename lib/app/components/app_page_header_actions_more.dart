e6c0905ebccfd6a3e2f445a3e1f9d777eef53931
wanghao <wanghao@ninghao.net>
2021-09-01 15:29:50 +0800

    设置与存储内容列表布局

--- lib/app/components/app_page_header_actions_more.dart
+++ lib/app/components/app_page_header_actions_more.dart
@@ -1,31 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xb2_flutter/post/index/post_index_model.dart';
 
 class AppPageHeaderActionsMore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final postIndexModel = context.watch<PostIndexModel>();
+
     return PopupMenuButton(
       icon: Icon(Icons.more_horiz),
       offset: Offset(0, 50),
       onCanceled: () {
         print('popupMenuButton: onCanceled');
       },
-      onSelected: (value) {
+      onSelected: (PostListLayout value) {
+        postIndexModel.storeLayout(value);
         print('popupMenuButton: onSelected $value');
       },
       itemBuilder: (context) => [
         PopupMenuItem(
           child: Icon(
             Icons.view_agenda_outlined,
-            color: Colors.black54,
+            color: postIndexModel.layout == PostListLayout.stack
+                ? Theme.of(context).primaryColor
+                : Colors.black,
           ),
-          value: 'stack',
+          value: PostListLayout.stack,
         ),
         PopupMenuItem(
           child: Icon(
-            Icons.dashboard_outlined,
-            color: Colors.black54,
+            Icons.grid_view,
+            color: postIndexModel.layout == PostListLayout.grid
+                ? Theme.of(context).primaryColor
+                : Colors.black,
           ),
-          value: 'grid',
+          value: PostListLayout.grid,
         ),
       ],
     );
