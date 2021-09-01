9d4d45445cc52d0fe9f073a197f51a1955acc052
wanghao <wanghao@ninghao.net>
2021-09-01 15:07:08 +0800

    准备内容列表布局相关数据与方法

--- lib/post/index/post_index_model.dart
+++ lib/post/index/post_index_model.dart
@@ -1,18 +1,37 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:xb2_flutter/app/app_config.dart';
 import 'package:xb2_flutter/app/app_service.dart';
 import 'package:xb2_flutter/post/post.dart';
 
+enum PostListLayout {
+  stack,
+  grid,
+}
+
 class PostIndexModel extends ChangeNotifier {
   List<Post>? posts;
   final AppService appService;
+  PostListLayout? layout = PostListLayout.stack;
 
   PostIndexModel({
     required this.appService,
     this.posts,
   });
+
+  setLayout(PostListLayout data) {
+    layout = data;
+    notifyListeners();
+  }
+
+  storeLayout(PostListLayout data) async {
+    setLayout(data);
+
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString('postListLayout', data.toString());
+  }
 
   List<Post> parsePosts(responseBody) {
     final List<Post> parsed = jsonDecode(responseBody)
