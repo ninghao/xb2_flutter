3937c273421b0b3fdd5976d4a30f25391ae80fdf
wanghao <wanghao@ninghao.net>
2021-09-03 22:27:09 +0800

    准备上传文件用的请求

--- lib/app/app_service.dart
+++ lib/app/app_service.dart
@@ -1,5 +1,8 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
+import 'package:xb2_flutter/app/app_config.dart';
 import 'package:xb2_flutter/auth/auth_model.dart';
 
 class ApiHttpClient extends http.BaseClient {
@@ -14,6 +17,25 @@ class ApiHttpClient extends http.BaseClient {
     if (token.isNotEmpty) {
       request.headers.putIfAbsent('Authorization', () => 'Bearer $token');
     }
+
+    return request.send();
+  }
+
+  Future<http.StreamedResponse> uploadImage({
+    required PlatformFile file,
+    required int postId,
+  }) async {
+    final uri = Uri.parse('${AppConfig.apiBaseUrl}/files?post=$postId');
+    final request = http.MultipartRequest('POST', uri);
+    request.headers.putIfAbsent('Autorization', () => 'Bearer $token');
+
+    final multipartFile = await http.MultipartFile.fromPath(
+      'file',
+      file.path,
+      contentType: MediaType('image', file.extension ?? 'jpg'),
+    );
+
+    request.files.add(multipartFile);
 
     return request.send();
   }
