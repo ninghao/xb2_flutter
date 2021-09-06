c794cdd480fce52085e737d618a97b7aaa84cde3
wanghao <wanghao@ninghao.net>
2021-09-06 10:13:23 +0800

    根据平台当前选择的主题来设置小部件的样式

--- lib/app/components/app_logo.dart
+++ lib/app/components/app_logo.dart
@@ -2,16 +2,30 @@ import 'package:flutter/material.dart';
 
 class AppLogo extends StatelessWidget {
   final double size;
-  final Color color;
+  final Color? color;
 
-  AppLogo({this.size = 32, this.color = Colors.white});
+  AppLogo({
+    this.size = 32,
+    this.color,
+  });
 
   @override
   Widget build(BuildContext context) {
+    Color _color;
+
+    final brightness = MediaQuery.of(context).platformBrightness;
+    final isDarkMode = brightness == Brightness.dark;
+
+    if (color == null) {
+      _color = isDarkMode ? Colors.grey : Colors.black;
+    } else {
+      _color = color!;
+    }
+
     return Image.asset(
       'assets/images/logo.png',
       width: size,
-      color: color,
+      color: _color,
     );
   }
 }
