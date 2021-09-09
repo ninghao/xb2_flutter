dc586d512efb672dbd78def3f221420aa7ed0b7f
wanghao <wanghao@ninghao.net>
2021-09-09 08:51:26 +0800

    准备创建用户表单小部件（UserCreateForm）

---
+++ lib/user/create/components/user_create_form.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:xb2_flutter/app/app_model.dart';
+import 'package:xb2_flutter/app/components/app_button.dart';
+import 'package:xb2_flutter/app/components/app_header_text.dart';
+import 'package:xb2_flutter/app/components/app_password_field.dart';
+import 'package:xb2_flutter/app/components/app_text_field.dart';
+import 'package:xb2_flutter/app/exceptions/http_exception.dart';
+import 'package:xb2_flutter/auth/auth.dart';
+import 'package:xb2_flutter/auth/auth_model.dart';
+import 'package:xb2_flutter/user/create/user_create_model.dart';
+
+class UserCreateForm extends StatefulWidget {
+  @override
+  _UserCreateFormState createState() => _UserCreateFormState();
+}
+
+class _UserCreateFormState extends State<UserCreateForm> {
+  String? name;
+  String? password;
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    final authModel = Provider.of<AuthModel>(context, listen: true);
+    final appModel = context.read<AppModel>();
+    final userCreateModel = context.read<UserCreateModel>();
+
+    // 标题
+    final header = AppHeaderText('注册用户');
+
+    // 用户
+    final nameField = AppTextField(
+      labelText: '用户',
+      onChanged: (value) {
+        name = value;
+      },
+    );
+
+    // 密码
+    final passwordField = AppPasswordField(
+      onChanged: (value) {
+        password = value;
+      },
+    );
+
+    // 提交
+    final submitButton = AppButton(
+      onPressed: () async {
+        formKey.currentState!.validate();
+
+        try {
+          await userCreateModel.createUser(
+            name: name!,
+            password: password!,
+          );
+
+          await authModel.login(
+            LoginData(
+              name: name!,
+              password: password!,
+            ),
+          );
+
+          appModel.setPageName('');
+        } on HttpException catch (e) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(e.message)),
+          );
+        }
+      },
+      text: '确定注册',
+    );
+
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: Form(
+        key: formKey,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            header,
+            nameField,
+            passwordField,
+            submitButton,
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/user/create/user_create.dart
+++ lib/user/create/user_create.dart
@@ -1,121 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:xb2_flutter/user/create/components/user_create_form.dart';
 
-class UserCreate extends StatefulWidget {
-  @override
-  _UserCreateState createState() => _UserCreateState();
-}
-
-class _UserCreateState extends State<UserCreate> {
-  String? name;
-  String? password;
-
-  final nameFieldKey = GlobalKey<FormFieldState>();
-  final passwordFieldKey = GlobalKey<FormFieldState>();
-
-  final nameFieldController = TextEditingController();
-  final passwordFieldController = TextEditingController();
-
-  final formKey = GlobalKey<FormState>();
-
-  @override
-  void dispose() {
-    super.dispose();
-
-    nameFieldController.dispose();
-    passwordFieldController.dispose();
-  }
-
-  @override
-  void initState() {
-    super.initState();
-
-    nameFieldController.addListener(() {
-      print('用户 ${nameFieldController.text}');
-    });
-
-    passwordFieldController.addListener(() {
-      print('密码 ${passwordFieldController.text}');
-    });
-
-    // nameFieldController.text = 'wanghao';
-    // passwordFieldController.text = '123456';
-  }
-
+class UserCreate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '注册用户',
-            style: TextStyle(
-              fontWeight: FontWeight.w300,
-              fontSize: 32,
-            ),
-          ),
-          SizedBox(height: 32),
-          TextFormField(
-            key: nameFieldKey,
-            controller: nameFieldController,
-            decoration: InputDecoration(
-              labelText: '用户',
-            ),
-            onChanged: (value) {
-              name = value;
-            },
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return '请填写用户名';
-              }
-
-              return null;
-            },
-          ),
-          SizedBox(height: 32),
-          TextFormField(
-            key: passwordFieldKey,
-            controller: passwordFieldController,
-            obscureText: true,
-            decoration: InputDecoration(
-              labelText: '密码',
-            ),
-            onChanged: (value) {
-              password = value;
-            },
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return '请填写用户密码';
-              }
-
-              if (value.isNotEmpty && value.length < 6) {
-                return '请设置 6 位以上的密码';
-              }
-
-              return null;
-            },
-          ),
-          SizedBox(height: 32),
-          ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              textStyle: TextStyle(fontSize: 20),
-              minimumSize: Size(double.infinity, 60),
-            ),
-            child: Text('注册用户'),
-            onPressed: () {
-              // nameFieldKey.currentState!.validate();
-              // passwordFieldKey.currentState!.validate();
-              formKey.currentState!.validate();
-              print(
-                  '注册用户：用户 ${nameFieldKey.currentState!.value}, 密码 ${passwordFieldKey.currentState!.value}');
-            },
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('注册用户'),
       ),
+      body: UserCreateForm(),
     );
   }
 }
